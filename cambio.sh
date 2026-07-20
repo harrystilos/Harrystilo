46ab7f82f5b7f0315a1e648103b71023e7bfbd45
harrystilos <sararicon13@gmail.com>
2026-07-19 23:37:00 -0500

    Automatizado: Actualización de galería 19-07-2026 23:36

--- cambio.sh
+++ cambio.sh
@@ -2,6 +2,6 @@
 echo "🚀 Subiendo cambios automáticamente a GitHub..."
 git add .
 git commit -m "Automatizado: Actualización de galería $(date +'%d-%m-%Y %H:%M')"
-git push origin main
+git push origin principal
 echo "🎉 ¡Todo listo! Tu web se está actualizando en internet."
 
