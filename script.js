fe9e5a165ebca4306900e93a4c3d6cb85b3475ec
harrystilos <sararicon13@gmail.com>
2026-07-19 21:49:41 -0500

    Rediseña hero, agrega reservas, galería y mejoras responsive

--- script.js
+++ script.js
@@ -32,3 +32,27 @@ ${comentarios}`;
     window.open(url, "_blank");
 
 });
+// ===== GALERÍA LIGHTBOX =====
+
+const imagenes = document.querySelectorAll(".image-container img");
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+const cerrar = document.querySelector(".close-lightbox");
+
+imagenes.forEach(img => {
+    img.addEventListener("click", () => {
+        lightbox.classList.add("active");
+        lightboxImg.src = img.src;
+        lightboxImg.alt = img.alt;
+    });
+});
+
+cerrar.addEventListener("click", () => {
+    lightbox.classList.remove("active");
+});
+
+lightbox.addEventListener("click", (e) => {
+    if (e.target === lightbox) {
+        lightbox.classList.remove("active");
+    }
+});
